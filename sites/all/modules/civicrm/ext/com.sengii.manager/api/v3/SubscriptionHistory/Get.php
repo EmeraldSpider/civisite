50c4eab71ee2a8ce3b40ab0d6df65dd03e65408c
Sean Kelly <emerald.spider.designs@gmail.com>
2018-12-05 20:24:58 -0600

    Final changes to the subscription history API call

--- sites/all/modules/civicrm/ext/com.sengii.manager/api/v3/SubscriptionHistory/Get.php
+++ sites/all/modules/civicrm/ext/com.sengii.manager/api/v3/SubscriptionHistory/Get.php
@@ -9,9 +9,6 @@ use CRM_Manager_ExtensionUtil as E;
  * @return void
  * @see http://wiki.civicrm.org/confluence/display/CRMDOC/API+Architecture+Standards
  */
-function _civicrm_api3_subscription_history_Get_spec(&$spec) {
-  //$spec['magicword']['api.required'] = 1;
-}
 
 /**
  * SubscriptionHistory.Get API
@@ -21,20 +18,7 @@ function _civicrm_api3_subscription_history_Get_spec(&$spec) {
  * @see civicrm_api3_create_success
  * @see civicrm_api3_create_error
  * @throws API_Exception
- */
+ */ 
 function civicrm_api3_subscription_history_Get($params) {
-  //if (array_key_exists('magicword', $params) && $params['magicword'] == 'sesame') {
-    $result = civicrm_api3('SubscriptionHistory', 'get', [
-        //'sequential' => 1,
-        //'date' => ['>=' => $params["date"]]
-      ]);
-    // ALTERNATIVE: $returnValues = array(); // OK, success
-    // ALTERNATIVE: $returnValues = array("Some value"); // OK, return a single value
-
-    // Spec: civicrm_api3_create_success($values = 1, $params = array(), $entity = NULL, $action = NULL)
-    return civicrm_api3_create_success($result, $params, 'NewEntity', 'NewAction');
-  //}
-  //else {
-  //  throw new API_Exception(/*errorMessage*/ 'Everyone knows that the magicword is "sesame"', /*errorCode*/ 1234);
-  //}
+  return _civicrm_api3_basic_get(_civicrm_api3_get_BAO(__FUNCTION__), $params, TRUE, 'SubscriptionHistory');
 }
